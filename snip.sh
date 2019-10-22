b82c25f66b3e507f3917af85a6e77c51aab7caaa
Juan Eugenio Abadie <juaneabadie@gmail.com>
2019-10-22 12:32:08 -0300

    Support writing the snip line as a comment to avoid linter errors

--- snip.sh
+++ snip.sh
@@ -12,7 +12,7 @@ __snip__replace_snips() {
   local source_file
   source_file="${1:?Missing source file as param}"
 
-  mapfile -t snippets < <(grep -Po '(?<=^snip\(")[^"]+' "$source_file")
+  mapfile -t snippets < <(grep -Po '(^|(?<=[^[:alnum:]]))(?<=snip\(")[^"]+' "$source_file")
   if (( ${#snippets[@]} == 0 )); then
     echo $source_file
     return
@@ -34,9 +34,9 @@ __snip__replace_snips() {
 
   local output_file=$prefix_tmp${extension:+.$extension}
   rm $prefix_tmp
-
   __snip__remove_snip_line "$source_file" > "$output_file"
   rm $prefix_tmp-*
+
   echo "$output_file"
 }
 
